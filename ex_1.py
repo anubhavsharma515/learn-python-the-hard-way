9b36261507a6ac694eca49464ada043a0e82ee16
Anubhav Sharma <anubhav.sharma@wattpad.com>
2022-09-24 13:15:06 -0400

    Add exercise_1 file

---
+++ ex_1.py
@@ -0,0 +1,14 @@
+print("hello world")
+print("hello again")
+print("I like typing this")
+print("this is fun")
+print("Yay printing!")
+print("I'd much rather you 'not'")
+print('I "said" do not touch this.')
+
+
+# print("This is how a comment works")
+print('''
+      This is a mutliline
+      string
+        ''')
