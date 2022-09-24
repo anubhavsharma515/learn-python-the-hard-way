1ec9aa3f8531a873a20da84264e16ad9c48b64a7
Anubhav Sharma <anubhav.sharma@wattpad.com>
2022-09-24 13:36:05 -0400

    Rename ex_2 to correct name ex_3.py

---
+++ ex_3.py
@@ -0,0 +1,10 @@
+print("I will now count chickens")
+
+print("Hens", 25+30/6)
+print("Roosters", 100-25 * 3 % 4)
+
+print("Now i will count eggs")
+
+print( 3+3+1-5+4%2-1/4+6 )
+
+print("is it greater?", 5 > -2)
