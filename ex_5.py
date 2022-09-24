286be9f9fdbf8ca950d6c17d8d38974896f0edfc
Anubhav Sharma <anubhav.sharma@wattpad.com>
2022-09-24 13:59:59 -0400

    Add exercise_5 file

---
+++ ex_5.py
@@ -0,0 +1,14 @@
+my_name = 'Anubhav Sharma'
+my_age = 23 #For now
+my_height = 175 # cm
+my_weight  = 63 # kgs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print("Let's talk about %s." % my_name)
+print("He's %d cm tall" % my_age)
+print("He's %d kgs heavy" % my_weight)
+print("%s needs some milk" % my_name)
+print("He's got %s eyes and %s hair" % (my_eyes, my_hair))
+print("His teeth are mostly %s depending on the coffee." % my_teeth)
