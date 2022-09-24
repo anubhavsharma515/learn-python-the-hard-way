49bee91fde2173ed6f00860d4dec1147ab99dbe4
Anubhav Sharma <anubhav.sharma@wattpad.com>
2022-09-24 14:15:04 -0400

    Add exercise_7 file

---
+++ ex_7.py
@@ -0,0 +1,22 @@
+print("Mary had a little lamb")
+print("It's fleece was white as %s" % 'snow')
+print("And everywhere that Mary went")
+print("." * 10)
+
+
+
+end1 = "C" 
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "y"
+end8 = "p"
+end9 = "i"
+end10 = "z"
+end11 = "z"
+end12 = "a"
+
+print(end1, end2, end3, end4, end5, end6, \
+end7, end8, end9, end10, end11, end12)
