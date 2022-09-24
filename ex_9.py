275135ac643d900019d2c1b65b6cef4600af7cd0
Anubhav Sharma <anubhav.sharma@wattpad.com>
2022-09-24 14:25:26 -0400

    Add exercise_9 file

---
+++ ex_9.py
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\n"
+
+print("Here are the days: ", days)
+print("Here are (some of) the months: ", months)
+
+print("""
+        There's something new here.
+        With the three quotes,
+        This is more than 1 line, wow!"
+      """)
+
