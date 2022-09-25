86c75ec7efba1cf72634598be0856eb4254c1e89
Anubhav Sharma <anubhav.sharma@wattpad.com>
2022-09-25 12:58:26 -0400

    Add exercise_12 file

---
+++ ex_12.py
@@ -0,0 +1,5 @@
+age = input("How old are ya?")
+weight = input("How much do ya weigh?")
+height = input("How tall are ya?")
+
+print("So you're %s, weigh %s kg and are %s cm tall." % (age, weight, height))
